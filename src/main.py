f2db214999e274aea8098a6765858a0ac0f06af1
MeinChef <126389277+MeinChef@users.noreply.github.com>
2025-05-15 22:23:39 +0200

    added lstm model, and lstm model with circular loss

--- src/main.py
+++ src/main.py
@@ -6,9 +6,27 @@ def main():
     cfg = data.load_config(os.path.join("cfg", "cfg.yml"))
     train, test = data.load_data(cfg)
     model = md.get_model()
+    model.summary()
     model.fit(train, epochs = cfg["epochs"])
     model.evaluate(test)
 
+    # TODO: nice visualisation of loss surfaces, and maybe interactive stuff
+
+def main_lstm():
+    cfg = data.load_config(os.path.join("cfg", "cfg.yml"))
+    train, test = data.load_lstm_data(cfg)
+    model = md.get_lstm_model()
+    model.summary()
+    model.fit(train, epochs = cfg["epochs"])
+    model.evaluate(test)
+
+def main_circular():
+    cfg = data.load_config(os.path.join("cfg", "cfg.yml"))
+    train, test = data.load_circ_data(cfg)
+    model = md.get_circ_model()
+    model.summary()
+    model.fit(train, epochs = cfg["epochs"])
+    model.evaluate(test)
 
 if __name__ == "__main__":
     main()
