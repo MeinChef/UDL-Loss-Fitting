26d2eb55b8b895fc938666a2a0413ab02e2455e6
MeinChef <126389277+MeinChef@users.noreply.github.com>
2025-07-17 17:00:42 +0200

    cleaned up code

--- src/model.py
+++ src/model.py
@@ -5,43 +5,39 @@ def get_dense_model() -> keras.Model:
     x = keras.layers.Dense(64, activation = "tanh")(inputs)
     x = keras.layers.Dense(32, activation = "tanh")(x)
     output = keras.layers.Dense(1)(x)
-    model = keras.models.Model(inputs=inputs, outputs=output)
-
-    # TODO: other loss function / custom loss function
-    # model.compile(optimizer = "adam", loss = "mse")
+    model = keras.models.Model(inputs = inputs, outputs = output)
     return model
 
-def get_lstm_model(seq_len=5, input_dim=2) -> keras.Model:
-    inputs = keras.layers.Input(shape=(seq_len, input_dim))
-    lstm_out = keras.layers.Bidirectional(keras.layers.LSTM(64, return_sequences=True))(inputs)
+def get_lstm_model(
+        seq_len = 5, 
+        input_dim = 3
+    ) -> keras.Model:
+    inputs = keras.layers.Input(shape = (seq_len, input_dim))
+    lstm_out = keras.layers.Bidirectional(keras.layers.LSTM(64, return_sequences = True))(inputs)
 
     # Attention mechanism
     attention_scores = keras.layers.Dense(1, activation='tanh')(lstm_out)
-    attention_weights = keras.layers.Softmax(axis=1)(attention_scores)
-    context_vector = keras.layers.Dot(axes=1)([attention_weights, lstm_out])
+    attention_weights = keras.layers.Softmax(axis = 1)(attention_scores)
+    context_vector = keras.layers.Dot(axes = 1)([attention_weights, lstm_out])
     context_vector = keras.layers.Flatten()(context_vector)
 
-    dense_out = keras.layers.Dense(32, activation='swish')(context_vector)
+    dense_out = keras.layers.Dense(32, activation = 'swish')(context_vector)
     output = keras.layers.Dense(1)(dense_out)
 
-    model = keras.models.Model(inputs=inputs, outputs=output)
-
-    # model.compile(optimizer='adam', loss='mse')
+    model = keras.models.Model(inputs = inputs, outputs = output)
     return model
 
-def get_circ_model(seq_len=5, input_dim=2):
-    inputs = keras.Input(shape=(seq_len, input_dim))
-    x = keras.layers.Bidirectional(keras.layers.LSTM(64, return_sequences=True))(inputs)
+def get_circ_model(seq_len = 5, input_dim = 2):
+    inputs = keras.Input(shape = (seq_len, input_dim))
+    x = keras.layers.Bidirectional(keras.layers.LSTM(64, return_sequences = True))(inputs)
     
-    attention = keras.layers.Dense(1, activation='tanh')(x)
-    attention = keras.layers.Softmax(axis=1)(attention)
-    context = keras.layers.Dot(axes=1)([attention, x])
+    attention = keras.layers.Dense(1, activation = 'tanh')(x)
+    attention = keras.layers.Softmax(axis = 1)(attention)
+    context = keras.layers.Dot(axes = 1)([attention, x])
     context = keras.layers.Flatten()(context)
 
-    x = keras.layers.Dense(32, activation="swish")(context)
+    x = keras.layers.Dense(32, activation = "swish")(context)
     output = keras.layers.Dense(2)(x)  # Predict sin & cos
 
     model = keras.Model(inputs, output)
-
-    # model.compile(optimizer='adam', loss=angle_cosine_loss)
     return model
