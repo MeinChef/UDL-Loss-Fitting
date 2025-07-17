f8fdf9582c6c3e16315a31838a9253c938701ef1
MeinChef <126389277+MeinChef@users.noreply.github.com>
2025-07-17 16:59:50 +0200

    added Cosine Similarity - doesn't work

--- src/loss.py
+++ src/loss.py
@@ -9,7 +9,7 @@ class VonMisesFisher(keras.losses.Loss):
     def __init__(
             self, 
             kappa: float = 1.0,
-            reduction = "sum_over_batch_size",
+            reduction:str = "sum_over_batch_size",
             name = "von_mises_fisher",
             dtype = tf.float32
         ):
@@ -17,13 +17,13 @@ class VonMisesFisher(keras.losses.Loss):
         Custom loss for von Mises-Fisher distribution, predicting $\mu$.
         The intuition behind kappa is that bigger values penalize angle deviations more.
         
-        :param kappa: concentration parameter (must be > 0)
+        :param kappa: Concentration parameter (must be > 0)
         :type kappa: float
 
-        :param reduction: type of reduction to apply to the loss
-        :type reduction: tf.keras.losses.Reduction
+        :param reduction: Type of reduction to apply to the loss. Available: none, sum, sum_over_batch_size. Default sum_over_batch_size
+        :type reduction: String
 
-        :param name: name of the loss function
+        :param name: Name of the loss function
         :type name: str
         """
 
@@ -41,5 +41,37 @@ class VonMisesFisher(keras.losses.Loss):
 
         # Dot product scaled by kappa
         dot_product = tf.reduce_sum(y_true * y_pred, axis = -1)
-        loss = -self.kappa * dot_product
+        loss = - self.kappa * dot_product
         return loss  # shape: (batch,)
+
+class CosineSimilarity(keras.losses.Loss):
+    def __init__(
+            self, 
+            reduction:str = "sum_over_batch_size",
+            name: str = "cosine_similairty_custom"
+        ):
+
+        """
+            A custom class for calculating the cosine similarity of two directions
+
+            :param reduction: Type of reduction to apply to the loss. Available: none, sum, sum_over_batch_size. Default sum_over_batch_size
+            :type reduction: String
+
+            :param name: Name of the loss function
+            :type name: str
+        """
+
+        super().__init__(
+            reduction = reduction,
+            name = name
+        )
+
+    @tf.function
+    def call(self, y_true, y_pred):
+        # normalize 
+        y_true = tf.math.l2_normalize(y_true, axis = -1)
+        y_pred = tf.math.l2_normalize(y_pred, axis = -1)
+
+        return tf.reduce_sum(tf.multiply(y_true, y_pred))
+
+        
