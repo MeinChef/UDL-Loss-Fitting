4d073521e756ad51f812cc50ccd3efe999a7755d
MeinChef <126389277+MeinChef@users.noreply.github.com>
2025-07-21 11:50:07 +0200

    Added new loss, based on the vM distribution

--- src/loss.py
+++ src/loss.py
@@ -73,5 +73,43 @@ class CosineSimilarity(keras.losses.Loss):
         y_pred = tf.math.l2_normalize(y_pred, axis = -1)
 
         return tf.reduce_sum(tf.multiply(y_true, y_pred))
+    
 
+class VonMises(keras.losses.Loss):
+    def __init__(
+            self, 
+            kappa: float = 1.0,
+            reduction:str = "sum_over_batch_size",
+            name = "von_mises_fisher",
+            dtype = tf.float32
+        ):
+
+        """
+        Custom loss for von Mises distribution, predicting $\mu$.
+        The intuition behind kappa is that bigger values penalize angle deviations more.
         
+        :param kappa: Concentration parameter (must be > 0)
+        :type kappa: float
+
+        :param reduction: Type of reduction to apply to the loss. Available: none, sum, sum_over_batch_size. Default sum_over_batch_size
+        :type reduction: String
+
+        :param name: Name of the loss function
+        :type name: str
+        """
+
+        super().__init__(
+            reduction = reduction, 
+            name = name
+        )
+
+        self.kappa = tf.convert_to_tensor(kappa, dtype = dtype)
+
+    @tf.function
+    def call(self, y_true, y_pred):
+        y_true = tf.math.l2_normalize(y_true, axis = -1)
+        y_pred = tf.math.l2_normalize(y_pred, axis = -1)
+
+        # 1 - ... because cosine is already between -1 and 1,
+        # thus this guarantees positivity.
+        return 1 - self.kappa * tf.math.cos(y_true - y_pred)
